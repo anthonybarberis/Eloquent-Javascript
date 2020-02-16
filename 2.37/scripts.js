d2a0e951ca7282303772effdbfd079d96d7d273d
Anthony Barberis <skamunism@gmail.com>
2020-02-16 12:20:49 -0700

    Improved labels

--- 2.37/scripts.js
+++
@@ -1,8 +0,0 @@
-let loopingATriangle = function () {
-    let triangle = '#######';
-    for (let i = 0; i < triangle.length; i++) {
-        console.log(triangle.substr(0, i + 1));
-    }
-}
-
-// loopingATriangle();
---
+++ scripts.js
@@ -0,0 +1,12 @@
+// 2.37
+
+let loopingATriangle = function () {
+    let triangle = '#######';
+    for (let i = 0; i < triangle.length; i++) {
+        console.log(triangle.substr(0, i + 1));
+    }
+}
+
+let fizzBuzz = function () {
+    
+}
